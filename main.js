feea3f9d3f5bcb865c27dca525c01dc51befbdcd
Dmitry Arkhipov <grisumbras@gmail.com>
2019-10-09 10:29:30 +0300

    fixed async/await

--- main.js
+++ main.js
@@ -4,46 +4,35 @@ const path = require('path');
 
 
 async function get_output(command, ...args) {
-  let output;
+  let output = '';
   const opts = {
     listeners: { stdout: (data) => { output += data.toString(); } }
   };
   await exec.exec(command, args, opts);
-  return output;
+
+  output = output.trim();
+  return 'None' != output ? output : '' ;
 }
 
 
-function get_pkg_name() {
-  let result = core.getInput('name');
-  if (!result) {
-    const output = get_output('conan', 'inspect', '.', '--raw', 'name').trim();
-    result = 'None' != output ? output : '' ;
-  }
+async function inspect_pkg(attr) {
+  return await get_output('conan', 'inspect', '.', '--raw', attr);
+}
+
+
+async function get_input_or_pkg_attr(attr) {
+  let result = core.getInput(attr);
+  if (!result) { result  = await inspect_pkg(attr); }
   return result;
 }
 
 
-function get_pkg_version() {
-  let result = core.getInput('version');
-  if (!result) {
-    const output
-      = get_output('conan', 'inspect', '.', '--raw', 'version').trim();
-    result = 'None' != output ? output : '' ;
-  }
-  return result;
-}
-
-
-function get_pkg_user() {
+async function get_pkg_user() {
   let result = core.getInput('user');
 
-  if (!result) { result = process.env['CONAN_USERNAME'].trim(); }
+  if (!result) { result = (process.env['CONAN_USERNAME'] || '').trim(); }
 
-  if (!result) {
-    const output
-      = get_output('conan', 'inspect', '.', '--raw', 'default_user').trim();
-    result = 'None' != output ? output : '' ;
-  }
+  if (!result) { result = await inspect_pkg('default_user'); }
 
   if (!result) {
     const repo = process.env['GITHUB_REPOSITORY'] || '';
@@ -54,44 +43,37 @@ function get_pkg_user() {
 }
 
 
-function get_default_channel() {
-  const output
-    = get_output('conan', 'inspect', '.', '--raw', 'default_channel').trim();
-  return 'None' != output ? output : '' ;
-}
-
-
-function get_pkg_channel() {
+async function get_pkg_channel() {
   let result = core.getInput('channel');
 
   if (!result) {
     const stable_channel
-      = process.env['CONAN_STABLE_CHANNEL'].trim()
+      = (process.env['CONAN_STABLE_CHANNEL'] || '').trim()
       || 'stable';
     const testing_channel
-      = process.env['CONAN_CHANNEL'].trim()
-      || get_default_channel()
+      = (process.env['CONAN_CHANNEL'] || '').trim()
+      || (await inspect_pkg('default_channel'))
       || 'testing';
 
-    const git_ref = process.env['GITHUB_REF'].split('/', 2)[2].trim();
+    const git_ref = process.env['GITHUB_REF'].split('/', 3)[2].trim();
     const stable_pattern
       = process.env['CONAN_STABLE_BRANCH_PATTERN'].trim()
       || '^(master$|release.*|stable.*)';
 
-    result = ref_name.match(stable_pattern) ? stable_channel : testing_channel;
+    result = git_ref.match(stable_pattern) ? stable_channel : testing_channel;
   }
 
   return result;
 }
 
 
-function get_pkg_reference() {
+async function get_pkg_reference() {
   let result = core.getInput('reference');
   if (!result) {
-    const name = get_pkg_name();
-    const version = get_pkg_version();
-    const channel = get_pkg_channel();
-    const user = get_pkg_user();
+    const name = await get_input_or_pkg_attr('name');
+    const version = await get_input_or_pkg_attr('version');
+    const user = await get_pkg_user();
+    const channel = await get_pkg_channel();
     result = `${name}/${version}@${user}/${channel}`
   }
   return result;
@@ -99,20 +81,21 @@ function get_pkg_reference() {
 
 
 function get_conan_home() {
-  let result = process.env['CONAN_USER_HOME'].trim()
+  let result = (process.env['CONAN_USER_HOME'] || '').trim()
   if (!result) {
     if ('win32' == process.platform || 'win64' == process.platform) {
-      conan_home = process.env['HOMEDRIVE'] + process.env['HOMEPATH']
+      result = process.env['HOMEDRIVE'] + process.env['HOMEPATH']
     } else {
-      conan_home = process.env['HOME']
+      result = process.env['HOME']
     }
   }
   return result;
 }
 
 
-function run() {
-  const pkg_reference = get_pkg_reference();
+async function run() {
+  const pkg_reference = await get_pkg_reference();
+  console.log()
   console.log(`Using full package reference ${pkg_reference}`);
 
   const conan_home = get_conan_home();
